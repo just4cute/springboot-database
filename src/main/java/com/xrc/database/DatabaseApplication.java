2fc317985d79a9b570f8fbcb9cd1d463605819a3
谢荣昌 <xierongchang@yodinfo.com>
2021-01-07 10:16:04 +0800

    feat:pom文件提交

--- src/main/java/com/xrc/database/DatabaseApplication.java
+++ src/main/java/com/xrc/database/DatabaseApplication.java
@@ -1,12 +1,18 @@
 package com.xrc.database;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xrc
+ */
 @SpringBootApplication
+@Slf4j
 public class DatabaseApplication {
 
     public static void main(String[] args) {
+        log.info("start project");
         SpringApplication.run(DatabaseApplication.class, args);
     }
 
